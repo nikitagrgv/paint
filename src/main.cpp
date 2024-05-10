d1378316bd2df437746c8f53675e46b78789aaec
Nikita Grigorev <pettyaelse@gmail.com>
2024-05-11 00:45:55 +0700

    w

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,185 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QPen>
+#include <QPainter>
+#include <QEvent>
+#include <QPaintEvent>
+#include <QTimer>
+#include <QOpenGLWindow>
+#include <QLabel>
+#include <QGridLayout>
+#include <QtOpenGLWidgets/QOpenGLWidget>
 
-int main(int argc, char* argv[])
-{
+class Helper {
+public:
+    Helper();
+
+public:
+    void paint(QPainter *painter, QPaintEvent *event, int elapsed);
+
+private:
+    QBrush background;
+    QBrush circleBrush;
+    QFont textFont;
+    QPen circlePen;
+    QPen textPen;
+};
+
+Helper::Helper() {
+    QLinearGradient gradient(QPointF(50, -20), QPointF(80, 20));
+    gradient.setColorAt(0.0, Qt::white);
+    gradient.setColorAt(1.0, QColor(0xa6, 0xce, 0x39));
+
+    background = QBrush(QColor(64, 32, 64));
+    circleBrush = QBrush(gradient);
+    circlePen = QPen(Qt::black);
+    circlePen.setWidth(1);
+    textPen = QPen(Qt::white);
+    textFont.setPixelSize(50);
+}
+
+void Helper::paint(QPainter *painter, QPaintEvent *event, int elapsed) {
+    painter->fillRect(event->rect(), background);
+    painter->translate(100, 100);
+
+    painter->save();
+    painter->setBrush(circleBrush);
+    painter->setPen(circlePen);
+    painter->rotate(elapsed * 0.030);
+
+    qreal r = elapsed / 1000.0;
+    int n = 30;
+    for (int i = 0; i < n; ++i) {
+        painter->rotate(30);
+        qreal factor = (i + r) / n;
+        qreal radius = 0 + 120.0 * factor;
+        qreal circleRadius = 1 + factor * 20;
+        painter->drawEllipse(QRectF(radius, -circleRadius,
+                                    circleRadius * 2, circleRadius * 2));
+    }
+    painter->restore();
+
+    painter->setPen(textPen);
+    painter->setFont(textFont);
+    painter->drawText(QRect(-50, -50, 100, 100), Qt::AlignCenter, QStringLiteral("Qt"));
+}
+
+class Helper;
+
+class Widget : public QWidget {
+    Q_OBJECT
+
+public:
+    Widget(Helper *helper, QWidget *parent);
+
+public slots:
+    void animate();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    Helper *helper;
+    int elapsed;
+};
+
+Widget::Widget(Helper *helper, QWidget *parent)
+    : QWidget(parent), helper(helper) {
+    elapsed = 0;
+    setFixedSize(200, 200);
+}
+
+void Widget::animate() {
+    elapsed = (elapsed + qobject_cast<QTimer *>(sender())->interval()) % 1000;
+    update();
+}
+
+void Widget::paintEvent(QPaintEvent *event) {
+    QPainter painter;
+    painter.begin(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    helper->paint(&painter, event, elapsed);
+    painter.end();
+}
+
+class Helper;
+
+class GLWidget : public QOpenGLWidget {
+    Q_OBJECT
+
+public:
+    GLWidget(Helper *helper, QWidget *parent);
+
+public slots:
+    void animate();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    Helper *helper;
+    int elapsed;
+};
+
+GLWidget::GLWidget(Helper *helper, QWidget *parent)
+    : QOpenGLWidget(parent), helper(helper) {
+    elapsed = 0;
+    setFixedSize(200, 200);
+    setAutoFillBackground(false);
+}
+
+void GLWidget::animate() {
+    elapsed = (elapsed + qobject_cast<QTimer *>(sender())->interval()) % 1000;
+    update();
+}
+
+void GLWidget::paintEvent(QPaintEvent *event) {
+    QPainter painter;
+    painter.begin(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    helper->paint(&painter, event, elapsed);
+    painter.end();
+}
+
+class Window : public QWidget {
+    Q_OBJECT
+
+public:
+    Window();
+
+private:
+    Helper helper;
+};
+
+Window::Window() {
+    setWindowTitle(tr("2D Painting on Native and OpenGL Widgets"));
+
+    Widget *native = new Widget(&helper, this);
+    GLWidget *openGL = new GLWidget(&helper, this);
+    QLabel *nativeLabel = new QLabel(tr("Native"));
+    nativeLabel->setAlignment(Qt::AlignHCenter);
+    QLabel *openGLLabel = new QLabel(tr("OpenGL"));
+    openGLLabel->setAlignment(Qt::AlignHCenter);
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(native, 0, 0);
+    layout->addWidget(openGL, 0, 1);
+    layout->addWidget(nativeLabel, 1, 0);
+    layout->addWidget(openGLLabel, 1, 1);
+    setLayout(layout);
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, native, &Widget::animate);
+    connect(timer, &QTimer::timeout, openGL, &GLWidget::animate);
+    timer->start(50);
+}
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
+    Window button;
     button.resize(200, 100);
     button.show();
     return QApplication::exec();
 }
+
+#include "main.moc"
